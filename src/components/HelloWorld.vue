dedf790caf7b359bd4568eb9a498d6e4283cc6e3
Miguel Herrero <miguelherrero@MacBook-de-Miguel.local>
2017-11-18 18:41:43 +0100

    Component for Thread items

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,45 +1,27 @@
 <template>
-  <div>
-    <div class="col-large push-top" v-for="thread in threads" :key="thread['.key']">
-      <h1>{{ thread.title }}</h1>
-      <div class="post-list">
-        <div class="post" v-for="postId in thread.posts" :key="postId">
-            <div class="user-info">
-                <a href="#" class="user-name">{{ users[posts[postId].userId].name }}</a>
-
-                <a href="#">
-                    <img class="avatar-large" :src="users[posts[postId].userId].avatar" alt="">
-                </a>
-
-                <p class="desktop-only text-small">107 posts</p>
-            </div>
-
-            <div class="post-content">
-                <div>
-                  <p>{{ posts[postId].text }}</p>
-                </div>
-            </div>
-
-            <div class="post-date text-faded">
-                {{ posts[postId].publishedAt }}
-            </div>
-        </div>
-      </div>
-    </div>
+  <div class="col-full">
+    <h1>Welcome to the Forum</h1>
+    <ThreadList :threads="threads" />
   </div>
 </template>
 
 <script>
 import sourceData from '@/data'
+import ThreadList from '@/components/ThreadList'
 
 export default {
   name: 'HelloWorld',
+
   data () {
     return {
-      threads: sourceData.threads,
+      threads: Object.values(sourceData.threads),
       posts: sourceData.posts,
       users: sourceData.users
     }
+  },
+
+  components: {
+    ThreadList
   }
 }
 </script>
